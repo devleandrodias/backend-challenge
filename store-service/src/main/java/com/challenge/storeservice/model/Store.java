db1c5478ceb66415781c391659966b3e6780136a
Jean Scapim <jeanscapim@gmail.com>
2019-10-14 20:53:31 -0300

    Added store model

--- store-service/src/main/java/com/challenge/storeservice/model/Store.java
+++ store-service/src/main/java/com/challenge/storeservice/model/Store.java
@@ -1,4 +1,37 @@
 package com.challenge.storeservice.model;
 
-public class Store {
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "store", schema = "challenge")
+public class Store implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Date createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        createdAt = new Date();
+    }
 }
