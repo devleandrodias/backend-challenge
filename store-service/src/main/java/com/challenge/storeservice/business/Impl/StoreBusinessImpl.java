24913f4c25a9c9fe5c7d1234f28c64028746a5fc
Jean Scapim <jeanscapim@gmail.com>
2019-10-14 20:58:23 -0300

    Added store business

--- store-service/src/main/java/com/challenge/storeservice/business/Impl/StoreBusinessImpl.java
+++ store-service/src/main/java/com/challenge/storeservice/business/Impl/StoreBusinessImpl.java
@@ -1,6 +1,48 @@
 package com.challenge.storeservice.business.Impl;
 
 import com.challenge.storeservice.business.StoreBusiness;
+import com.challenge.storeservice.exception.StoreNotFoundException;
+import com.challenge.storeservice.model.Store;
+import com.challenge.storeservice.repository.StoreRepository;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 public class StoreBusinessImpl implements StoreBusiness {
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Override
+    public Optional<Store> create(@NonNull Store store) {
+        return Optional.of(storeRepository.saveAndFlush(store));
+    }
+
+    @Override
+    public Optional<List<Store>> read() {
+        return Optional.of(storeRepository.findAll());
+    }
+
+    @Override
+    public Optional<Store> update(@NonNull Store store) {
+        storeRepository.findById(store.getId())
+                .orElseThrow(StoreNotFoundException::new);
+
+        return Optional.of(storeRepository.saveAndFlush(store));
+    }
+
+    @Override
+    public void delete(@NonNull Integer id) {
+        storeRepository.findById(id)
+                .orElseThrow(StoreNotFoundException::new);
+
+        storeRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Store> findById(@NonNull Integer id) {
+        return Optional.of(storeRepository.findById(id)
+                .orElseThrow(StoreNotFoundException::new));
+    }
 }
--- store-service/src/main/java/com/challenge/storeservice/business/StoreBusiness.java
+++ store-service/src/main/java/com/challenge/storeservice/business/StoreBusiness.java
@@ -1,4 +1,15 @@
 package com.challenge.storeservice.business;
 
+import com.challenge.storeservice.model.Store;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface StoreBusiness {
+    Optional<Store> create(@NonNull final Store store);
+    Optional<List<Store>> read();
+    Optional<Store> update(@NonNull final Store store);
+    void delete(@NonNull final Integer id);
+    Optional<Store> findById(@NonNull final Integer id);
 }
